fefd771de2e48d7f3a063ab86867ba80bc205e6e
Edwin Kepler <kepler.c@protonmail.com>
2021-11-02 09:53:27 +0100

    Add Exercism/cpp/series

--- Exercism/cpp/series/series.h
+++ Exercism/cpp/series/series.h
@@ -7,42 +7,8 @@
 using namespace std;
 
 namespace series {
-	vector<int> digits(string _s)
-	{
-		vector<int> v;
-
-		for(auto& c: _s)
-		{
-			v.push_back(stoi(to_string(c)) - 48);
-		}
-
-		return v;
-	}
-
-	vector<vector<int>> slice(string _s, int _n)
-	{
-		if((int)_s.length() < _n)
-		{
-			throw domain_error("you deserve whatever you get");
-		}
-
-		vector<vector<int>> r;
-		vector<int> v = series::digits(_s);
-
-		for(unsigned int i = 0; i <= v.size() - _n; i++)
-		{
-			vector<int> temp;
-
-			for(int j = 0; j < _n; j++)
-			{
-				temp.push_back(v[i + j]);
-			}
-
-			r.push_back(temp);
-		}
-
-		return r;
-	}
+	vector<int> digits(string);
+	vector<vector<int>> slice(string, int);
 }  // namespace series
 
 #endif // SERIES_H
