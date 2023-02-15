9c830863e3038239e0bac238e5a4bcaf07e4f8d5
Edwin Jablonski <kepler.c@protonmail.com>
2023-02-15 17:11:32 +0100

    Refactor Exercism/typescript/diffie-hellman

--- Exercism/typescript/diffie-hellman/diffie-hellman.ts
+++ Exercism/typescript/diffie-hellman/diffie-hellman.ts
@@ -1,8 +1,6 @@
 export class DiffieHellman {
-  private sieve: boolean[] = this.primes(9999)
-
   constructor(private p: number, private g: number) {
-    if(this.sieve[this.p] !== true || this.sieve[this.g] !== true) {
+    if(this.isPrime(this.p) !== true || this.isPrime(this.g) !== true) {
       throw Error('Arguments not a prime numbers')
     }
   }
@@ -19,39 +17,16 @@ export class DiffieHellman {
     return (theirPublicKey ** myPrivateKey) % this.p
   }
 
-  primes(_n: number): boolean[] {
-    // 0 and 1 are not primes so 'false' them right away
-    let primes_b: boolean[] = [false, false]
+  private isPrime(n: number): boolean {
+    let is_prime = true
 
-    for(let i = 2; i <= _n; i++) {
-      primes_b.push(true)
-    }
-
-    // Eratosthenes sieve
-    for(let i = 2; i <= Math.trunc(Math.sqrt(_n)); i++) {
-      if(primes_b[i] === true) {
-        let is_prime = true
-
-        for(var j = 2; j <= i; j++) {
-          if(i % j === 0 && i !== j) {
-            is_prime = false
-            break
-          }
-        }
-
-        if(is_prime) {
-          primes_b[i] = true
-          primes_b[i * i] = false
-
-          for(var k = 1; (i * i) + (i * k) <= _n; k++) {
-            primes_b[(i * i) + (i * k)] = false
-          }
-        } else {
-          primes_b[i] = false
+      for(let i = 2; i < n; i++) {
+        if(n % i === 0) {
+          is_prime = false
+          break
         }
       }
-    }
 
-    return primes_b
+    return is_prime
   }
 }
