9d914eaef8e09972d72961d700a8ad8d0dfe37b4
Edwin Jablonski <kepler.c@protonmail.com>
2023-02-23 12:19:02 +0100

    Refactor Exercism/typescript/atbash-cipher

--- Exercism/typescript/atbash-cipher/atbash-cipher.ts
+++ Exercism/typescript/atbash-cipher/atbash-cipher.ts
@@ -1,11 +1,6 @@
-// Latin alphabet Atbash cipher
-const CIPHER: [string, string][] = [
-  ['a', 'z'], ['b', 'y'], ['c', 'x'], ['d', 'w'], ['e', 'v'], ['f', 'u'],
-  ['g', 't'], ['h', 's'], ['i', 'r'], ['j', 'q'], ['k', 'p'], ['l', 'o'],
-  ['m', 'n'], ['n', 'm'], ['o', 'l'], ['p', 'k'], ['q', 'j'], ['r', 'i'],
-  ['s', 'h'], ['t', 'g'], ['u', 'f'], ['v', 'e'], ['w', 'd'], ['x', 'c'],
-  ['y', 'b'], ['z', 'a']
-]
+const ALPHABET: string = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const CIPHER: string = 'zyxwvutsrqponmlkjihgfedcba0123456789'
+const PUNCT_AND_NUMS_REGEX = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-./:;<=>?@[\]^_`{|}~]/g;
 
 /**
  * Atbash ciphers a string. Adds space character every 5 characters for extra security.
@@ -13,40 +8,7 @@ const CIPHER: [string, string][] = [
  * @returns Encoded string.
  */
 export function encode(s: string): string {
-  const PUNCT_REGEX = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-./:;<=>?@[\]^_`{|}~]/g;
-
-  var ret: string[] = []
-
-  s.replace(PUNCT_REGEX, '')
-    .replace(/ /g, '')
-    .toLowerCase()
-    .split('')
-    .forEach(el => {
-      let found = false
-
-      for(let pair of CIPHER) {
-        if(pair[0] === el) {
-          ret.push(pair[1])
-          found = true
-          break
-        }
-      }
-
-      if(!found) ret.push(el)
-    })
-
-  for (var i = 0, a = []; i < ret.length; i++) {
-    a.push(ret[i]);
-    if ((i + 1) % 5 === 0) {
-      a.push(' ');
-      i++;
-      if(ret[i]) a.push(ret[i]);
-    }
-  }
-
-  ret = a
-
-  return ret.join('').trim()
+  return atbash(s, ALPHABET, CIPHER).replace(/(.{5})/g, '$1 ').trim()
 }
 
 /**
@@ -55,23 +17,22 @@ export function encode(s: string): string {
  * @returns Decoded string.
  */
 export function decode(s: string): string {
-  var ret: string[] = []
-
-  s.replace(/ /g, '')
+  return atbash(s, CIPHER, ALPHABET)
+}
+/**
+ * Map string characters from one alphabet to another.
+ * @param s String to be mapped.
+ * @param from Alphabet that string s is written in.
+ * @param to Alphabet to map to.
+ * @returns Mapped string.
+ */
+function atbash(s: string, from: string, to: string): string {
+  return s
+    .replace(PUNCT_AND_NUMS_REGEX, '')
+    .toLowerCase()
     .split('')
-    .forEach(el => {
-      let found = false
-
-      for(let pair of CIPHER) {
-        if(pair[1] === el) {
-          ret.push(pair[0])
-          found = true
-          break
-        }
-      }
-
-      if(!found) ret.push(el)
+    .map((el) => {
+      return to[from.indexOf(el)]
     })
-
-  return ret.join('')
+    .join('')
 }
