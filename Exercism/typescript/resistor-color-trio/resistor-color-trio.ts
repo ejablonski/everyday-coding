b3bca8815b23fa1518c631b8c924706af655e7e1
Edwin Jablonski <kepler.c@protonmail.com>
2023-02-24 10:43:06 +0100

    Refactor Exercism/typescript/resistor-color-trio

--- Exercism/typescript/resistor-color-trio/resistor-color-trio.ts
+++ Exercism/typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,58 +1,19 @@
-export function decodedResistorValue(_vals: string[]) {
-	let ret = ""
+const values = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+const prefixes = ['', 'kilo', 'mega', 'giga']
 
-	for(let i = 0; i < _vals.length - 1; i++) {
-		ret += getVal(_vals[i])
-	}
-
-	for(let j = 0; j < Number(getVal(_vals[_vals.length - 1])); j++) {
-		ret += "0"
-	}
-
-	if(Number(ret) > 1000) {
-		ret = (Number(ret) / 1000).toString()
-		ret += " kiloohms"
-	} else {
-		ret += " ohms"
-	}
-
-	return ret
+export function decodedResistorValue(vals: string[]): string {
+  return reduceToMetric(Number(vals.map((e, i) => {
+    return (i < 2 ? values.indexOf(e) : null)
+  }).join('')) * (10 ** values.indexOf(vals[2])))
 }
 
-function getVal(_val: string) {
-	switch(_val) {
-		case "black":
-			return "0"
-			break
-		case "brown":
-			return "1"
-			break
-		case "red":
-			return "2"
-			break
-		case "orange":
-			return "3"
-			break
-		case "yellow":
-			return "4"
-			break
-		case "green":
-			return "5"
-			break
-		case "blue":
-			return "6"
-			break
-		case "violet":
-			return "7"
-			break
-		case "grey":
-			return "8"
-			break
-		case "white":
-			return "9"
-			break
-		default:
-			return "-1"
-			break
-	}
+function reduceToMetric(n: number): string {
+  let p = 0
+
+  while(n > 1000) {
+    n /= 1000
+    p++
+  }
+
+  return `${n.toString()} ${prefixes[p]}ohms`
 }
