3df4d2f0bb6f7402e18e88642888f8e68f63ea92
Edwin Jablonski <kepler.c@protonmail.com>
2023-02-24 09:13:25 +0100

    Refactor Exercism/typescript/resistor-color-duo

--- Exercism/typescript/resistor-color-duo/resistor-color-duo.ts
+++ Exercism/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,47 +1,5 @@
-export function decodedValue(_vals: string[]) {
-	let ret = ""
+const values = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
 
-	for(let i = 0; i < 2; i++) {
-		ret += getVal(_vals[i])
-	}
-
-	return Number(ret)
-}
-
-function getVal(_val: string) {
-	switch(_val) {
-		case "black":
-			return "0"
-			break
-		case "brown":
-			return "1"
-			break
-		case "red":
-			return "2"
-			break
-		case "orange":
-			return "3"
-			break
-		case "yellow":
-			return "4"
-			break
-		case "green":
-			return "5"
-			break
-		case "blue":
-			return "6"
-			break
-		case "violet":
-			return "7"
-			break
-		case "grey":
-			return "8"
-			break
-		case "white":
-			return "9"
-			break
-		default:
-			return "-1"
-			break
-	}
+export function decodedValue(v: string[]): number {
+  return Number(v.map((e) => { return values.indexOf(e) }).slice(0, 2).join(''))
 }
