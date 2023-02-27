be3f65a8fcd030f6be0473597d90b3c4ab844d5f
Edwin Jablonski <kepler.c@protonmail.com>
2023-02-27 08:44:20 +0100

    Refactor Exercism/typescript/grade-school

--- Exercism/typescript/grade-school/grade-school.test.ts
+++ Exercism/typescript/grade-school/grade-school.test.ts
@@ -63,11 +63,11 @@ describe('School', () => {
   })
 
   it('roster cannot be modified outside of module', () => {
-	school.add('Aimee', 2)
-	const roster = school.roster()
-	roster[2].push('Oops.')
-	const expectedDb = { 2: ['Aimee'] }
-	expect(school.roster()).toEqual(expectedDb)
+    school.add('Aimee', 2)
+    const roster = school.roster()
+    roster[2].push('Oops.')
+    const expectedDb = { 2: ['Aimee'] }
+    expect(school.roster()).toEqual(expectedDb)
   })
 
   it('roster cannot be modified outside of module using grade()', () => {
--- Exercism/typescript/grade-school/grade-school.ts
+++ Exercism/typescript/grade-school/grade-school.ts
@@ -1,35 +1,30 @@
 export class GradeSchool {
-	_roster: { [key: number]: string[] } = {}
+  _roster: { [key: number]: string[] } = {}
 
-	roster(): { [key: number]: string[] } {
-		const ret: { [key: number]: string[] } = structuredClone(this._roster)
-		return ret
-	}
+  roster(): { [key: number]: string[] } {
+    return structuredClone(this._roster)
+  }
 
-	add(_name: string, _grade: number): void {
-		Object.entries(this._roster).forEach(
-			([k, v]) => {
-				if(v.includes(_name)) {
-					v.splice(v.indexOf(_name), 1)
-				}
-			}
-		)
+  add(name: string, grade: number): void {
+    Object.entries(this._roster).forEach(
+      ([k, v]) => {
+        if(v.includes(name)) {
+          v.splice(v.indexOf(name), 1)
+        }
+      }
+    )
 
-		if(this._roster[_grade] === undefined) {
-			this._roster[_grade] = new Array(_name)
-		} else {
-			this._roster[_grade].push(_name)
-			this._roster[_grade].sort((a, b) => a.localeCompare(b))
-		}
-	}
+    if(this._roster[grade] === undefined) {
+      this._roster[grade] = new Array(name)
+    } else {
+      this._roster[grade].push(name)
+      this._roster[grade].sort((a, b) => a.localeCompare(b))
+    }
+  }
 
-	grade(_grade: number): string[] {
-		let ret: string[]
-
-		this._roster[_grade] === undefined
-			? ret = new Array() 
-			: ret = [...this._roster[_grade]]
-
-		return ret
-	}
+  grade(grade: number): string[] {
+    return this._roster[grade] === undefined
+      ? new Array() 
+      : [...this._roster[grade]]
+  }
 }
