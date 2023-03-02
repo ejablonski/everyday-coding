14a6bae6f92f39560abb9690e30ae079b3b2d6bd
Edwin Jablonski <kepler.c@protonmail.com>
2023-03-02 07:53:21 +0100

    Refactor Exercism/typescript/secret-handshake

--- Exercism/typescript/secret-handshake/secret-handshake.ts
+++ Exercism/typescript/secret-handshake/secret-handshake.ts
@@ -1,32 +1,27 @@
-export function commands(_n: number) {
-	if(_n == 0)
-		return []
+export function commands(n: number): string[] {
+  let ret: string[] = []
+  let rev = false
+  let binary = parseInt(n.toString(2), 10)
 
-	let ret: string[] = []
-	let rev: boolean = false
-	let n: number = parseInt(_n.toString(2), 10)
 
-	while(n >= 1) {
-		if(n - 10000 >= 0) {
-			rev = true
-			n -= 10000
-		} else if(n - 1000 >= 0) {
-			ret.push("jump")
-			n -= 1000
-		} else if(n - 100 >= 0) {
-			ret.push("close your eyes")
-			n -= 100
-		} else if(n - 10 >= 0) {
-			ret.push("double blink")
-			n -= 10
-		} else if(n - 1 >= 0) {
-			ret.push("wink")
-			n -= 1
-		}
-	}
+  while(binary >= 1) {
+    if(binary - 10000 >= 0) {
+      rev = true
+      binary -= 10000
+    } else if(binary - 1000 >= 0) {
+      ret.push("jump")
+      binary -= 1000
+    } else if(binary - 100 >= 0) {
+      ret.push("close your eyes")
+      binary -= 100
+    } else if(binary - 10 >= 0) {
+      ret.push("double blink")
+      binary -= 10
+    } else if(binary - 1 >= 0) {
+      ret.push("wink")
+      binary -= 1
+    }
+  }
 
-	if(!rev)
-		ret.reverse()
-
-	return ret;
+  return !rev ? ret.reverse() : ret
 }
