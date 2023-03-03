c2c3f5ad74f05f3ed76616109c9f68a9b6808115
Edwin Jablonski <kepler.c@protonmail.com>
2023-03-03 08:38:40 +0100

    Refactor Exercism/cpp/queen-attack

--- Exercism/cpp/queen-attack/queen_attack.cpp
+++ Exercism/cpp/queen-attack/queen_attack.cpp
@@ -1,74 +1,68 @@
 #include "queen_attack.h"
 
 namespace queen_attack {
-	chess_board::chess_board()
-	{
-		whiteQ = {0, 3};
-		blackQ = {7, 3};
-	}
+  chess_board::chess_board() {
+    whiteQ = {0, 3};
+    blackQ = {7, 3};
+  }
 
-	chess_board::chess_board(std::pair<int, int> _w = {0, 3}, std::pair<int, int> _b = {7, 3})
-	{
-		if(_w == _b)
-		{
-			throw std::domain_error("no");
-		}
+  chess_board::chess_board(
+    std::pair<int, int> _w = {0, 3},
+    std::pair<int, int> _b = {7, 3}
+  ) {
+    if(
+      _w == _b ||
+      _w.first < 0 ||
+      _w.first > 7 ||
+      _w.second < 0 ||
+      _w.second > 7 ||
+      _b.first < 0 ||
+      _b.first > 7 ||
+      _b.second < 0 ||
+      _b.second > 7
+    ) {
+      throw std::domain_error("no");
+    }
 
-		whiteQ = _w;
-		blackQ = _b;
-	}
+    whiteQ = _w;
+    blackQ = _b;
+  }
 
-	std::pair<int, int> chess_board::white() const
-	{
-		return whiteQ;
-	}
+  std::pair<int, int> chess_board::white() const {
+    return whiteQ;
+  }
 
-	std::pair<int, int> chess_board::black() const
-	{
-		return blackQ;
-	}
+  std::pair<int, int> chess_board::black() const {
+    return blackQ;
+  }
 
-	bool chess_board::can_attack() const
-	{
-		if(whiteQ.first == blackQ.first || whiteQ.second == blackQ.second)
-		{
-			return true;
-		}
-		else if(std::abs(whiteQ.first - blackQ.first) == std::abs(whiteQ.second - blackQ.second))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+  bool chess_board::can_attack() const {
+    if(whiteQ.first == blackQ.first || whiteQ.second == blackQ.second) {
+      return true;
+    } else if(std::abs(whiteQ.first - blackQ.first) == std::abs(whiteQ.second - blackQ.second)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	chess_board::operator std::string() const
-	{
-		std::string b = "";
+  chess_board::operator std::string() const {
+    std::string b = "";
 
-		for(int i = 0; i < 8; i++)
-		{
-			for(int j = 0; j < 8; j++)
-			{
-				if(whiteQ.first == i && whiteQ.second == j)
-				{
-					b += "W ";
-				}
-				else if(blackQ.first == i && blackQ.second == j)
-				{
-					b += "B ";
-				}
-				else
-				{
-					j != 7 ? b += "_ " : b += "_";
-				}
-			}
+    for(int i = 0; i < 8; i++) {
+      for(int j = 0; j < 8; j++) {
+        if(whiteQ.first == i && whiteQ.second == j) {
+          b += "W ";
+        } else if(blackQ.first == i && blackQ.second == j) {
+          b += "B ";
+        } else {
+          j != 7 ? b += "_ " : b += "_";
+        }
+      }
 
-			b += "\n";
-		}
+      b += "\n";
+    }
 
-		return b;
-	}
+    return b;
+  }
 }  // namespace queen_attack
--- Exercism/cpp/queen-attack/queen_attack.h
+++ Exercism/cpp/queen-attack/queen_attack.h
@@ -1,27 +1,23 @@
-#if !defined(QUEEN_ATTACK_H)
-#define QUEEN_ATTACK_H
+#pragma once
 
 #include <stdexcept>
 #include <string>
 #include <utility>
 
 namespace queen_attack {
-	class chess_board
-	{
-	public:
-		chess_board();
-		chess_board(std::pair<int, int>, std::pair<int, int>);
+  class chess_board {
+  public:
+    chess_board();
+    chess_board(std::pair<int, int>, std::pair<int, int>);
 
-		std::pair<int, int> white() const;
-		std::pair<int, int> black() const;
-		bool can_attack() const;
+    std::pair<int, int> white() const;
+    std::pair<int, int> black() const;
+    bool can_attack() const;
 
-		operator std::string() const;
+    operator std::string() const;
 
-	private:
-		std::pair<int, int> whiteQ;
-		std::pair<int, int> blackQ;
-	};
+  private:
+    std::pair<int, int> whiteQ;
+    std::pair<int, int> blackQ;
+  };
 }  // namespace queen_attack
-
-#endif // QUEEN_ATTACK_H
