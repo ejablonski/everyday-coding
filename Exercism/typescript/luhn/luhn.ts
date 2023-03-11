c75bc41e1b9355a4694ad843321410a8daaa1319
Edwin Jablonski <kepler.c@protonmail.com>
2023-03-11 10:26:03 +0100

    Refactor Exercism/typescript/luhn

--- Exercism/typescript/luhn/luhn.ts
+++ Exercism/typescript/luhn/luhn.ts
@@ -1,21 +1,25 @@
-export function valid(_s: string): boolean {
-	_s = _s.replace(/\s+/g, '').replace('-', '')
+export function valid(str: string): boolean {
+  str = str.replace(/\s+/g, '').replace('-', '')
 
-	if(_s.length <= 1)
-		return false
+  if(str.length <= 1) return false
 
-	let sum: number = 0
+  let sum: number = 0
+  let j = 1
 
-	for(let i = _s.length - 1; i >= 0; i--) {
-		let n: number = parseInt(_s[i])
-		if(i % 2) {  
-			n *= 2
-			if(n > 9) {
-			  n -= 9
-			}
-		}
-		sum += n
-	}
+  for(let i = str.length - 1; i >= 0; i--) {
+    let n: number = Number(str[i])
 
-	return (sum % 10 == 0)
+    if(j % 2 === 0) {
+      n *= 2
+
+      if(n > 9) {
+        n -= 9
+      }
+    }
+
+    sum += n
+    j++
+  }
+
+  return (sum % 10 == 0)
 }
