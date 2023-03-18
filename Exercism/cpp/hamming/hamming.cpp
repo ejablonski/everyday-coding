f140eaa0a737fc79f78142a40707d6002485df3c
Edwin Jablonski <kepler.c@protonmail.com>
2023-03-18 18:34:58 +0100

    Refactor Exercism/cpp/hamming

---
+++ Exercism/cpp/hamming/hamming.cpp
@@ -0,0 +1,16 @@
+#include "hamming.h"
+
+namespace hamming {
+  int compute(string a, string b) {
+    if(a.length() != b.length())
+        throw domain_error("Different string sizes");
+
+    int n = 0;
+    for(unsigned int i = 0; i < a.length(); i++) {
+      if(a[i] != b[i])
+          n++;
+    }
+
+    return n;
+  }
+}  // namespace hamming
--- Exercism/cpp/hamming/hamming.h
+++ Exercism/cpp/hamming/hamming.h
@@ -1,21 +1,8 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-namespace hamming
-{
-	int compute(string a, string b)
-	{
-		if(a.length() != b.length())
-			throw domain_error("Different string sizes");
+namespace hamming {
+  using namespace std;
 
-		int n = 0;
-		for(unsigned int i = 0; i < a.length(); i++)
-		{
-			if(a[i] != b[i])
-				n++;
-		}
-
-		return n;
-	}
+  int compute(string, string);
 }
